fb09d7f90d935aa91d7444f92a8a53bae5b566f4
TonyCode2012 <TonyCode2012@126.com>
2018-01-14 23:49:14 +0800

    submit changed database

--- app/v2/db/marking.sql
+++ app/v2/db/marking.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 50720
  Source Host           : localhost:3306
- Source Schema         : marking
+ Source Schema         : cAuth
 
  Target Server Type    : MySQL
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 13/01/2018 19:48:36
+ Date: 14/01/2018 23:48:47
 */
 
 SET NAMES utf8mb4;
@@ -63,6 +63,25 @@ CREATE TABLE `DelegationShip`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for DelegatorInfo
+-- ----------------------------
+DROP TABLE IF EXISTS `DelegatorInfo`;
+CREATE TABLE `DelegatorInfo`  (
+  `open_id` varchar(40) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `gender` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `identity_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `wechat` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `phone_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`open_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of DelegatorInfo
+-- ----------------------------
+INSERT INTO `DelegatorInfo` VALUES ('oHEkW0dNJwwjb3ete07iObpWGFcM', 'yaoz', 'male', '522636', 'wechat', '15021128363');
+
+-- ----------------------------
 -- Table structure for MatchContract
 -- ----------------------------
 DROP TABLE IF EXISTS `MatchContract`;
@@ -102,20 +121,28 @@ CREATE TABLE `S2DPush`  (
 DROP TABLE IF EXISTS `SeekerInfo`;
 CREATE TABLE `SeekerInfo`  (
   `open_id` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  `gender` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `identity_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `wechat` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `phone_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `life_photo` mediumblob NULL,
-  `age` int(1) NULL DEFAULT NULL,
-  `height` int(1) NULL DEFAULT NULL,
+  `height` int(11) NULL DEFAULT NULL,
   `requirement` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `portrait` mediumblob NULL,
   `self_introduction` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `reward` float(1, 0) NULL DEFAULT NULL,
+  `reward` float NULL DEFAULT NULL,
   `constellation` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `blood_type` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `education` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`open_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of SeekerInfo
+-- ----------------------------
+INSERT INTO `SeekerInfo` VALUES ('oHEkW0dNJwwjb3ete07iObpWGFcM', 'yaoz', 18, 'male', '522636', 't6865733', '15021128363', 0x5B5D, 183, 'optimistc life attitude, volubility, independence and brave.', NULL, 'brave, like fitness, cooking and basketball, sometimes I like taking a travel with my friends.', 400, 'baiyang', 'O', 'postgraduate');
 
 -- ----------------------------
 -- Table structure for User
@@ -126,14 +153,38 @@ CREATE TABLE `User`  (
   `public_key` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `chain_addr` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `balance` float(255, 0) NOT NULL,
-  `gender` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `register_time` datetime(0) NOT NULL,
   `identity_hash` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `status` int(255) NOT NULL,
-  `wechat` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `phone_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` int(255) NOT NULL DEFAULT 0,
   `role` int(255) NOT NULL,
   PRIMARY KEY (`open_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of User
+-- ----------------------------
+INSERT INTO `User` VALUES ('oHEkW0dNJwwjb3ete07iObpWGFcM', 'oHEkW0dNJwwjb3ete07iObpWGFcM12345', 'oHEkW0dNJwwjb3ete07iObpWGFcM98765', 100, '2018-01-14 17:30:03', 'oHEkW0dNJwwjb3ete07iObpWGFcMyaoz', 0, 0);
+
+-- ----------------------------
+-- Table structure for cSessionInfo
+-- ----------------------------
+DROP TABLE IF EXISTS `cSessionInfo`;
+CREATE TABLE `cSessionInfo`  (
+  `open_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `skey` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_visit_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `session_key` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_info` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`open_id`) USING BTREE,
+  INDEX `openid`(`open_id`) USING BTREE,
+  INDEX `skey`(`skey`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '会话管理用户信息' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cSessionInfo
+-- ----------------------------
+INSERT INTO `cSessionInfo` VALUES ('oHEkW0dNJwwjb3ete07iObpWGFcM', 'e247af0b-c986-49b3-80e9-9566ebf5fe29', '0d423ce129af8253d19908dc26059914e5afe431', '2018-01-14 23:39:34', '2018-01-14 23:39:34', '9BwhU202pjTKrgHgEqSRiw==', '{\"openId\":\"oHEkW0dNJwwjb3ete07iObpWGFcM\",\"nickName\":\"yo\",\"gender\":1,\"language\":\"zh_CN\",\"city\":\"Minhang\",\"province\":\"Shanghai\",\"country\":\"China\",\"avatarUrl\":\"https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIurjgzyviayzePjJr1VWzsiaCd0OBxqsY7hOSPqrcQ2s8xOsSU21Nuwf5QxcDz5W74eMYIOUe9j54w/0\",\"watermark\":{\"timestamp\":1515944374,\"appid\":\"wx8727802679966793\"}}');
 
 SET FOREIGN_KEY_CHECKS = 1;

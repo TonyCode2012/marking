c216c390843d97b27b0301b7bf70e6f1d9ef3c2b
TonyCode2012 <TonyCode2012@126.com>
2018-03-14 23:06:02 +0800

    change add scene script

--- app/v2/scripts/addScene.sh
+++ app/v2/scripts/addScene.sh
@@ -4,13 +4,16 @@ function insertUser()
     local file="$1"
     local idType=""
     local userType=""
+    local role=""
     local lineNum=`cat $pf | wc -l`
     if [[ "$file" =~ seeker ]]; then 
         userType="客户"
-        idType="seeker"
+        idType="index"
+        role="seeker"
     else
         userType="红娘"
-        idType="delegator"
+        idType="index"
+        role="delegator"
     fi
     lineNum=$((lineNum-4))
     for openId in `cat $file`; do
@@ -23,16 +26,24 @@ function insertUser()
         ((lineNum++))
         sed -i "${lineNum} a\\\t\t\t\t\t\"pathName\": \"pages\/$idType\/$idType\"," $pf
         ((lineNum++))
-        sed -i "${lineNum} a\\\t\t\t\t\t\"query\": \"openId=$openId\"," $pf
+        sed -i "${lineNum} a\\\t\t\t\t\t\"query\": \"openId=$openId&role=$role\"," $pf
         ((lineNum++))
-        sed -i "${lineNum} a\\\t\t\t\t\t\"scene\": 1037," $pf
+        sed -i "${lineNum} a\\\t\t\t\t\t\"scene\": 1044," $pf
         ((lineNum++))
-        sed -i "${lineNum} a\\\t\t\t\t\t\"referrerInfo\": {" $pf
+        sed -i "${lineNum} a\\\t\t\t\t\t\"shareInfo\": {" $pf
         ((lineNum++))
-        sed -i "${lineNum} a\\\t\t\t\t\t\t\"appId\": \"wx8727802679966793\"," $pf
+        sed -i "${lineNum} a\\\t\t\t\t\t\t\"groupName\": \"测试模拟群0\"," $pf
         ((lineNum++))
-        sed -i "${lineNum} a\\\t\t\t\t\t\t\"extraData\": \"{\\\\\"name\\\\\":\\\\\"$openId\\\\\"}\"" $pf
+        sed -i "${lineNum} a\\\t\t\t\t\t\t\"shareName\": \"okSKrJHju7CriIhADtobc-1BRhHpqYwim8D_DgkmC-E@cr4dev\"" $pf
         ((lineNum++))
+        sed -i "${lineNum} a\\\t\t\t\t\t\t\"shareKey\": \"6ctu4CIRcLgiV5nPxjWYyHpr-OHy-_IuDkHamwYmG_g63vF97x35bDapbc9eOAhLIPaU875HGGGitrQrBL-jsg~~\"" $pf
+        ((lineNum++))
+        #sed -i "${lineNum} a\\\t\t\t\t\t\"referrerInfo\": {" $pf
+        #((lineNum++))
+        #sed -i "${lineNum} a\\\t\t\t\t\t\t\"appId\": \"wx8727802679966793\"," $pf
+        #((lineNum++))
+        #sed -i "${lineNum} a\\\t\t\t\t\t\t\"extraData\": \"{\\\\\"name\\\\\":\\\\\"$openId\\\\\"}\"" $pf
+        #((lineNum++))
         sed -i "${lineNum} a\\\t\t\t\t\t}" $pf
         ((lineNum++))
         sed -i "${lineNum} a\\\t\t\t\t}" $pf
